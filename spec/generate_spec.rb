e91c6502a02ad0a0368ab32cff93b138d03b49d1
Dunya Kirkali <dunyakirkali@gmail.com>
2015-12-26 19:32:04 +0100

    Clean up specs

--- spec/generate_spec.rb
+++ spec/generate_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 RSpec.describe 'VCard', type: :model do
   describe 'generate' do
-    it 'should create 2 vcards' do
-      `ruby generate.rb`
-      expect(FileUtils.getwd('build').count).to eq(2)
-    end
-
+    it 'should create 2 vcards'
     it 'the created file should have .vcard as extension'
   end
 end
